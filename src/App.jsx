c22c7b61d5e0b597e3eb6d9d823bed298f5f1cec
Heytemisan <temyicy@gmail.com>
2022-01-12 10:49:58 +0100

    updated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import './App.css';
-import {Shop, Pricelist, Benefit, Learn} from './components/index'
+import React from "react";
+import "./App.css";
+import { Shop, Pricelist, Benefit, Learn, Home } from "./components/index";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
-      <Shop/>
-      <Pricelist/>
-      <Benefit/>
-      <Learn/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/shop" element={<Shop/>}/>
+          <Route path="/pricelist" element={<Pricelist/>}/>
+          <Route path="/benefit" element={<Benefit/>}/>
+          <Route path="/learn" element={<Learn/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as Benefit} from './pages/Benefit';
 export {default as Learn} from './pages/Learn';
 export {default as Shop} from './pages/Shop';
 export {default as Pricelist} from './pages/Pricelist';
+export {default as Home} from './pages/Home';
