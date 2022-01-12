01e73adb7386c46676b9e0abae6557f0edd171d5
Heytemisan <temyicy@gmail.com>
2022-01-12 18:41:04 +0100

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    background-color: black;
-    color: white;
+    color: #1f4037;
     height: 60px;
     display:flex;
     align-items:center
 `
 const Left = styled.div`
+    flex:3;
+    padding-left:40px;
+`
+const Right = styled.div`
+    flex:7;
+    padding-right:40px;
+    display:flex;
+    justify-content:flex-end;
+`
+
+const NavbarContainer = styled.div`
+
+`
+
+const NavbarLink = styled(NavLink)`
+    text-decoration:none;
+    font-size:16px;
+    color:#1f4037;
+    margin:5px;
+    padding:0 10px;
+`
+
+const Logo = styled.h1`
 
 `
 
 const Navbar = () => {
     return (
         <Container>
-            nav
+            <Left>
+                <Logo>Logo</Logo>
+            </Left>
+            <Right>
+                <NavbarContainer>
+                    <NavbarLink to="/">Home</NavbarLink>
+                    <NavbarLink to="/pricelist">pricelist</NavbarLink>
+                    <NavbarLink to="/benefit">benefit</NavbarLink>
+                    <NavbarLink to="/learn">learn</NavbarLink>
+                    <NavbarLink to="/learn"><a>Hello</a></NavbarLink>
+                </NavbarContainer>
+            </Right>
         </Container>
     )
 }
